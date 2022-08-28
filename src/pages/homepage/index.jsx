66ad807c20a9c4ef2908f70671c4221058e67ad9
mohamed bebba <mohamedbebba1@gmail.com>
2022-08-28 18:32:53 +0200

    new changes

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../componnets/navbar";
 import "./homepage.scss";
+import burger from "../../assets/burger.png";
 const Homrpage = () => {
   return (
     <div className="container">
-      <Navbar />
+      <Navbar />!
+      <img className="burger" src={burger} alt="burger" />
     </div>
   );
 };
